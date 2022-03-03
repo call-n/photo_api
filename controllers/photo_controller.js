c4d1a81588060ec6eb24bbc4d12c8239368b5dd6
call-n <callenilsson2001@gmail.com>
2022-03-03 17:52:21 +0100

    the finished controller for photo

---
+++ controllers/photo_controller.js
@@ -0,0 +1,92 @@
+const { matchedData, validationResult } = require('express-validator');
+const models = require('../models');
+ 
+const index = async (req, res) => {
+    const photos = await models.Photo.fetchAll();
+ 
+    res.send({
+        status: 'success',
+        data: photos,
+    });
+}
+ 
+const show = async (req, res) => {
+    const photo = await new models.Photo({ id: req.params.photoId })
+        .fetch();
+ 
+    res.send({
+        status: 'success',
+        data: photo,
+    });
+}
+ 
+const store = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).send({ status: 'fail', data: errors.array() });
+    }
+ 
+    const validData = matchedData(req);
+ 
+    try {
+        const photo = await new models.Photo(validData).save();
+       
+        res.send({
+            status: 'success',
+            data: photo,
+        });
+ 
+    } catch (error) {
+        res.status(500).send({
+            status: 'error',
+            message: 'Exception thrown in database when creating a new photo.',
+        });
+        throw error;
+    }
+}
+ 
+const update = async (req, res) => {
+    const photoId = req.params.photoId;
+
+    // make sure photo exists
+    const photo = await new models.Photo({ id: photoId }).fetch({ require: false });
+    if (!photo) {
+        res.status(404).send({
+            status: 'fail',
+            data: 'Photo Not Found',
+        });
+        return;
+    }
+ 
+    const errors = validationResult(req);
+     
+    if (!errors.isEmpty()) {
+        return res.status(422).send({ status: 'fail', data: errors.array() });
+    }
+ 
+    const validData = matchedData(req);
+ 
+    try {
+        const updatedPhoto = await photo.save(validData);
+
+        res.send({
+             status: 'success',
+             data: updatedPhoto,
+        });
+ 
+    } catch (error) {
+        res.status(500).send({
+            status: 'error',
+            message: 'Exception thrown in database when updating a new photo.',
+        });
+        throw error;
+    }
+}
+ 
+module.exports = {
+    index,
+    show,
+    store,
+    update,
+}
+ 
